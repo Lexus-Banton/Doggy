0bac7b21fd2a035806cc380543b7bde3d5fdf357
Lexus Banton <lexusbanton.kr@gmail.com>
2022-08-08 17:59:57 -0700

    It works!

--- detail/detail.js
+++ detail/detail.js
@@ -4,6 +4,13 @@ import { renderDogDetail } from '../render-utils.js';
 const dogDetailContainer = document.getElementById('dog-detail-container');
 
 // on load
+const star = new URLSearchParams(window.location.search);
+async function loadData() {
+    const data = await getDog(star.get('id'));
+    const dogDiv = renderDogDetail(data);
+    dogDetailContainer.append(dogDiv);
+}
+loadData();
 // get the id from URL
 // use the id to fetch the dog
 // render and append this dog's details to the container
